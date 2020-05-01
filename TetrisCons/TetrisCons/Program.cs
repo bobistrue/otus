5647262434262be92df048098037b1ed2f4ee9f8
bobistrue <bobistrue@gmail.com>
2020-05-01 13:41:17 +0500

    Создали проект

---
+++ TetrisCons/TetrisCons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TetrisCons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
