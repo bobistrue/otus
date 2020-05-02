dc84b03439181cc995c923b3a8538ef03b474f64
bobistrue <bobistrue@gmail.com>
2020-05-02 18:41:18 +0500

    Printing 2 points on screen

--- TetrisCons/TetrisCons/Program.cs
+++ TetrisCons/TetrisCons/Program.cs
@@ -6,7 +6,23 @@ namespace TetrisCons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Tetris";
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 0;
+            int y1 = 1;
+            char c1 = '*';
+
+            Draw(x1, y1, c1);
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
