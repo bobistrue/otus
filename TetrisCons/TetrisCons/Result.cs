5c45e95f9b7da5ec5362d4f38d17a073f7932112
bobistrue <bobistrue@gmail.com>
2020-05-04 14:44:54 +0500

    Drawer and Result

---
+++ TetrisCons/TetrisCons/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetrisCons
+{
+    enum Result
+    {
+        BORDER_STRIKE
+        , DOWN_BORDER_STRIKE
+        , HEAP_STRIKE
+        , SUCCESS
+    }
+}
