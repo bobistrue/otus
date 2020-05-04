7c516316ac8c5078bbee3bacf654e6c1f5b3ea47
bobistrue <bobistrue@gmail.com>
2020-05-04 21:07:29 +0500

    +

---
+++ TetrisCons/TetrisCons/ConsoleDrawer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetrisCons
+{
+    class ConsoleDrawer : IDrawer
+    {
+        public void DrawPoint(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write('o');
+            Console.SetCursorPosition(0,0);
+        }
+
+        public void HidePoint(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(' ');
+            Console.SetCursorPosition(0, 0);
+        }
+
+        public void InitGameField()
+        {
+            Console.Title = "Tetris";
+            Console.SetWindowSize(GameField.Width, GameField.Height);
+            Console.SetBufferSize(GameField.Width, GameField.Height);
+        }
+
+        public void WriteGameOver()
+        {
+            Console.SetCursorPosition(GameField.Width / 2 - 8, GameField.Height / 2 - 2);
+            Console.WriteLine("G A M E   O V E R");
+        }
+    }
+}
---
+++ TetrisCons/TetrisCons/DrawerProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetrisCons
+{
+    static class DrawerProvider
+    {
+        static IDrawer _drawer = new ConsoleDrawer();
+
+        public static IDrawer Drawer { get { return _drawer; } }
+    }
+}
